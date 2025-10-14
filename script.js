a1d32f415b5aeaff972142e56dee0f592e181bc4
jiri-slanina <jirislanina96@gmail.com>
2025-10-14 18:18:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function zobrazZpravu() {
     let emailInput = document.querySelector('input[type="email"]')
     let email = emailInput.value;
 
+    
+
     if(!email || !email.includes("@")) {
         document.getElementById("odeslat").innerHTML = "⚠️ Chyba ⚠️ <br> Zkontrolujte své údaje.";
 
